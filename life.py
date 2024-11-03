6c9340a0e8367c11e959b907f74797cc0cd20321
S21B0069 <96568240+S21B0069@users.noreply.github.com>
2024-11-03 12:49:02 +0800

    Update life.py

--- life.py
+++ life.py
@@ -1,5 +1,3 @@
 import pandas as pd
 data = pd.read_csv('https://raw.githubusercontent.com/S21B0069/new-project-DS/refs/heads/main/Life%20Expectancy%20Data.csv')
-data.head()
-
-data.isnull().sum()
+st.write(df)
