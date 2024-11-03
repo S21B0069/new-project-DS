a8b4070b1db876b63e8289767ce490cb1236ab89
S21B0069 <96568240+S21B0069@users.noreply.github.com>
2024-11-03 13:09:53 +0800

    Update life.py

--- life.py
+++ life.py
@@ -10,3 +10,26 @@ df.isnull().sum()
 
 df = pd.DataFrame(df)
 del df['GDP']
+
+df.isnull().sum()
+
+df.dropna(inplace=True)
+
+df.isnull().sum()
+df.shape
+df.describe()
+
+import matplotlib.pyplot as plt
+# Creating dataset
+data=df['Adult Mortality']
+fig = plt.figure(figsize =(10, 7))
+
+# Creating plot
+plt.boxplot(data)
+
+# show plot
+plt.show()
+
+
+# Now you can use plt to create figures and plots:
+fig = plt.figure(figsize=(10, 7))
