5274639f725ac9326dbe22f67444c2aff9c674a6
S21B0069 <96568240+S21B0069@users.noreply.github.com>
2024-11-03 13:11:59 +0800

    Update life.py

--- life.py
+++ life.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv('https://raw.githubusercontent.com/S21B0069/new-project-DS/refs/heads/main/Life%20Expectancy%20Data.csv')
-st.write(data)
+df = pd.read_csv('https://raw.githubusercontent.com/S21B0069/new-project-DS/refs/heads/main/Life%20Expectancy%20Data.csv')
+st.write(df)
 
 # Checking for Null Values
 df.isnull().sum()
