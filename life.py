a2a6567a9923ef880de0de16d9da4e09c3728f3f
S21B0069 <96568240+S21B0069@users.noreply.github.com>
2024-11-03 12:50:06 +0800

    Update life.py

--- life.py
+++ life.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import pandas as pd
 data = pd.read_csv('https://raw.githubusercontent.com/S21B0069/new-project-DS/refs/heads/main/Life%20Expectancy%20Data.csv')
 st.write(df)
